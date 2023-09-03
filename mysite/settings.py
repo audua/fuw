4d040373023c8bfb52e852080cdc971d05134b64
auduab <auduab@gmail.com>
2023-09-03 15:14:05 +0100

    add csrf in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,6 +113,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+CSRF_TRUSTED_ORIGINS = [
+    'fuw-auduab.b4a.run'
+]
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
