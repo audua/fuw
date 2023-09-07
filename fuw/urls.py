439620338ab1841f21c28fd63fba76d75838a98a
auduab <auduab@gmail.com>
2023-09-07 06:23:12 +0100

    restruct the views

--- fuw/urls.py
+++ fuw/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from fuw.views import StudentDetailView, IndexView
+from fuw.views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('student-detail/<path:matric_number>/', StudentDetailView.as_view(), name='student-detail'),
 ]
--- fuw/views.py
+++ fuw/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import DetailView, FormView
+from django.views.generic import DetailView, FormView, ListView
 from fuw.models import Student
 from fuw.forms import StudentForm
 
@@ -11,14 +11,7 @@ class IndexView(FormView):
 
     def form_valid(self, form):
         matric_number = form.cleaned_data['matric_number'].upper()
-        self.success_url = reverse('fuw:student-detail', kwargs={'matric_number': matric_number})
-        return super().form_valid(form)
-
-
-class StudentDetailView(DetailView):
-
-    model = Student
-    template_name = 'fuw/student.html'
-    context_object_name = 'student'
-    slug_url_kwarg = "matric_number"
-    slug_field = "matric_number"
+        context = self.get_context_data()
+        context['matric_number'] = matric_number
+        context['students'] = Student.objects.filter(matric_number=matric_number)
+        return render(self.request, 'fuw/student.html', context)
